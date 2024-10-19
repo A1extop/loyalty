2644075d0fcbec61ad4549a316b95340cc54042c
A1extop <blednykh1995@inbox.ru>
2024-10-19 20:06:20 +0300

    изменение взаимодействия с системой начисления баллов

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/A1extop/loyalty/config"
 	http2 "github.com/A1extop/loyalty/internal/http"
@@ -24,8 +23,7 @@ func main() {
 		psql.CreateOrConnectTable(db)
 	}
 	router := http2.NewRouter(repos)
-	ticker := time.NewTicker(time.Duration(parameters.Interval))
-	go repos.InteractionWithCalculationSystem(ticker, parameters.SystemAddr)
+
 	log.Printf("Starting server on port %s", parameters.AddressHTTP)
 	err = http.ListenAndServe(parameters.AddressHTTP, router)
 	if err != nil {
