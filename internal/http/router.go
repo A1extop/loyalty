76dde544d3b8b038374290e01eea64526a20d58e
A1extop <blednykh1995@inbox.ru>
2024-10-23 22:38:03 +0300

    добавляю возможность сжимания данных

--- internal/http/router.go
+++ internal/http/router.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"github.com/A1extop/loyalty/internal/compress"
 	"github.com/A1extop/loyalty/internal/logging"
 	"github.com/gin-gonic/gin"
 )
@@ -12,13 +13,13 @@ type ReposHandler struct {
 func NewRouter(repos *Repository) *gin.Engine {
 	router := gin.New()
 	log := logging.New()
-	router.POST("/api/user/register", logging.LoggingPost(log), repos.Register)
-	router.POST("/api/user/login", logging.LoggingPost(log), repos.Authentication)
-	router.POST("api/user/orders", logging.AuthMiddleware(), logging.LoggingPost(log), repos.Loading)
-	router.GET("/api/user/orders", logging.AuthMiddleware(), logging.LoggingGet(log), repos.GetOrders)
-	router.GET("/api/user/balance", logging.AuthMiddleware(), logging.LoggingGet(log), repos.GetBalance)
-	router.POST("/api/user/balance/withdraw", logging.AuthMiddleware(), logging.LoggingPost(log), repos.PointsDebiting)
-	router.GET("/api/user/withdrawals", logging.AuthMiddleware(), logging.LoggingGet(log), repos.GetWithdrawals)
+	router.POST("/api/user/register", compress.CompressData(), logging.LoggingPost(log), repos.Register)
+	router.POST("/api/user/login", compress.DeCompressData(), logging.LoggingPost(log), repos.Authentication)
+	router.POST("api/user/orders", compress.CompressData(), logging.AuthMiddleware(), logging.LoggingPost(log), repos.Loading)
+	router.GET("/api/user/orders", compress.DeCompressData(), logging.AuthMiddleware(), logging.LoggingGet(log), repos.GetOrders)
+	router.GET("/api/user/balance", compress.DeCompressData(), logging.AuthMiddleware(), logging.LoggingGet(log), repos.GetBalance)
+	router.POST("/api/user/balance/withdraw", compress.CompressData(), logging.AuthMiddleware(), logging.LoggingPost(log), repos.PointsDebiting)
+	router.GET("/api/user/withdrawals", compress.DeCompressData(), logging.AuthMiddleware(), logging.LoggingGet(log), repos.GetWithdrawals)
 
 	return router
 }
